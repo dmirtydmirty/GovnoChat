657381edc4833ff14657bd7e90cac5bd22a7ba00
Антонов Дмитрий Игоревич <107511435+dmirtydmirty@users.noreply.github.com>
2024-12-22 13:06:04 +0300

    Srart write scripts for linux

---
+++ Scripts/start.sh
@@ -0,0 +1,8 @@
+source <(grep VERSION .ini)
+source <(grep PORT .ini)
+source <(grep CONTAINER_NAME .ini)
+source <(grep LOG_FILE .ini)
+
+rm $LOG_FILE
+
+docker run --name $CONTAINER_NAME -p $PORT:15001 --rm ghcr.io/dmirtydmirty/govnochat:$VERSION &
